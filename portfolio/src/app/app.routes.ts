d0d3befaa2b6e95776abf23c4202f7c9230512b3
Giano <bfcnorbi11@gmail.com>
2026-02-12 23:40:45 +0100

    Version 0.2.3: Skills és Documents oldalak eltávolítása az oldalról

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -2,8 +2,6 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ProjectListComponent } from './components/project-list/project-list.component';
 import { ProjectDetailComponent } from './components/project-detail/project-detail.component';
-import { SkillsComponent } from './components/skills/skills.component';
-import { DocumentsComponent } from './components/documents/documents.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { ExperienceComponent } from './components/experience/experience.component';
 
@@ -11,8 +9,6 @@ export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'projects', component: ProjectListComponent },
   { path: 'projects/:id', component: ProjectDetailComponent },
-  { path: 'skills', component: SkillsComponent },
-  { path: 'documents', component: DocumentsComponent },
   { path: 'experience', component: ExperienceComponent },
   { path: 'contact', component: ContactComponent },
   { path: '**', redirectTo: '' }
