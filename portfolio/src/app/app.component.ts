3d94be898b8b7417f59a0ab28dbaa890d221ac1d
Giano <bfcnorbi11@gmail.com>
2026-03-08 23:52:01 +0100

    Version 0.5.4: Scrollbar bug javítása

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BugReportComponent } from './components/bug-report/bug-report.component';
+import { ViewportScroller } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +14,12 @@ import { BugReportComponent } from './components/bug-report/bug-report.component
 })
 export class AppComponent {
   title = 'portfolio';
+
+  constructor(private router: Router, private viewportScroller: ViewportScroller) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.viewportScroller.scrollToPosition([0, 0]);
+      });
+  }
 }
